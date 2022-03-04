c71efcd47d23fb4e455bc76f5027ae11f24c85c5
Alina-KI <kraynova.alina.2002@mail.ru>
2022-03-04 10:42:06 +0300

    fixed about

--- src/components/pages/about/about.tsx
+++ src/components/pages/about/about.tsx
@@ -2,16 +2,24 @@ import React from 'react'
 import './about.scss'
 import {AboutInfo} from "./about-info/about-info";
 import {aboutInfos} from "../../../content/about";
+import start from '../../../images/Slider.svg'
+import social from '../../../images/Social.svg'
 
 
 export const AboutPage = () => {
     return (
         <div className='about'>
             <section className="about__top">
-                <div className="about__top__inner">
-                    <h3 className="about__sub-title">About Escape.</h3>
-                    <h1 className="about__title">Be Prepared
-                        For The Mountains And Beyond!</h1>
+                <div className="about__container container">
+                    <a href="" className="about__img">
+                        <img className="about__img__social" src={social} alt=""/>
+                    </a>
+                    <div className="about__top__inner">
+                        <h3 className="about__sub-title">About Escape.</h3>
+                        <h1 className="about__title">Be Prepared
+                            For The Mountains And Beyond!</h1>
+                    </div>
+                    <img className="about__img__start" src={start} alt=""/>
                 </div>
             </section>
             <div className="about__top__gradient"/>
